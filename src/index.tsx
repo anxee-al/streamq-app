11da9b62a053801a6ce0beb3435e0005833775ba
DXLETED <xdeleted.al@gmail.com>
2021-11-02 17:46:07 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+import st from 'styles/index.sass'
+
+const App: React.FC = () => {
+  return <>
+  </>
+}
+
+ReactDOM.render(<App />, document.getElementsByTagName('root')[0])
---
+++ webpack.dev.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    main: './src/index.tsx'
+  },
+  mode: 'development',
+  output: {
+    filename: `js/app.js`,
+    path: path.resolve(__dirname, 'dist/web/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: { loader: 'ts-loader' },
+        resolve: {
+          fullySpecified: false
+        }
+      },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.sass$/i,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { modules: { localIdentName: '[local]_[hash:base64:5]' } } },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+    modules: [ path.resolve(__dirname, 'src'), "node_modules"]
+  },
+  target: 'electron-renderer',
+  plugins: [
+    new HtmlWebpackPlugin({ inject: 'body', scriptLoading: 'blocking', meta: { charset: 'UTF-8' } }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  watch: true,
+  devtool: 'source-map'
+}
